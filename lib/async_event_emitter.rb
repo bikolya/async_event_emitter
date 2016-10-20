249660e1d20b8e9cb51a6203f0df58962443ecd5
Nikolay Bienko <bikolya@gmail.com>
2016-10-21 02:16:46 +0300

    Add basic EventEmitter with observe/notify/reset methods

--- lib/async_event_emitter.rb
+++ lib/async_event_emitter.rb
@@ -1,5 +1,41 @@
 require "async_event_emitter/version"
+require "forwardable"
 
-module AsyncEventEmitter
-  # Your code goes here...
+class AsyncEventEmitter
+  class << self
+    extend Forwardable
+    def_delegators :instance, :notify, :observe, :events, :reset
+
+    def instance
+      @instance ||= new
+    end
+  end
+
+  attr_reader :events
+
+  def initialize
+    @events = default_hash
+  end
+
+  def notify(event, data = {})
+    events[event.to_sym].each do |subscriber:|
+      subscriber.public_send(event, data)
+    end
+  end
+
+  def observe(event, subscriber)
+    events[event.to_sym] << { subscriber: subscriber }
+  end
+
+  def reset
+    @events = default_hash
+  end
+
+  private
+
+  def default_hash
+    Hash.new { |hash, key| hash[key] = [] }
+  end
 end
+
+EventEmitter = AsyncEventEmitter
--- spec/async_event_emitter_spec.rb
+++ spec/async_event_emitter_spec.rb
@@ -1,11 +1,56 @@
 require "spec_helper"
 
 describe AsyncEventEmitter do
-  it "has a version number" do
-    expect(AsyncEventEmitter::VERSION).not_to be nil
+  before do
+    described_class.reset
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#observe' do
+    subject { described_class.observe(event, subscriber) }
+    let(:event) { :some_event }
+    let(:subscriber) { double(:subscriber) }
+
+    context 'when there are no custom options' do
+      it 'adds new subscriber for events collection' do
+        subject
+        expect(described_class.events).to eq({
+          some_event: [{ subscriber: subscriber }]
+        })
+      end
+    end
+  end
+
+  describe '#notify' do
+    subject { described_class.notify(event, data) }
+    let(:event) { :some_event }
+    let(:data) do
+      { order_id: 1 }
+    end
+    let(:subscriber) { double(:subscriber) }
+
+    before do
+      described_class.observe(event, subscriber)
+    end
+
+    it 'notifies subscribers' do
+      expect(subscriber).to receive(:some_event).with(data)
+      subject
+    end
+  end
+
+  describe '#reset' do
+    subject { described_class.reset }
+    let(:event) { :some_event }
+    let(:subscriber) { double(:subscriber) }
+
+    before do
+      described_class.observe(event, subscriber)
+    end
+
+    it 'resets events collection' do
+      expect(described_class.events).not_to eq({})
+      subject
+      expect(described_class.events).to eq({})
+    end
   end
 end
